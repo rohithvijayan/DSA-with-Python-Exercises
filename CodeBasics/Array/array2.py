9c6229ddcaf49d419ac7c5787b206dc853446059
rohithvijayan <rohithvij20@gmail.com>
2024-07-15 23:07:24 +0530

    codebasics-array

---
+++ CodeBasics/Array/array2.py
@@ -0,0 +1,10 @@
+#2 input a max number from user and create a list containing odd numbers between 1 and max
+max = int(input("Enter max number: "))
+
+odd_numbers = []
+
+for i in range(1, max):
+    if i % 2 == 1:
+        odd_numbers.append(i)
+
+print("Odd numbers: ", odd_numbers)
--- LinkedList/append.py
+++ LinkedList/append.py
@@ -27,8 +27,30 @@ class LinkedList:
             self.tail.next=new_node
             self.tail=new_node
         self.length+=1
+    def pop(self):
+        if self.head is None:
+            return None
+        elif(self.head.next is None):
+            self.head=None
+            self.tail=None
+            self.length=0
+        else:
+            temp=self.head
+            pre=self.head
+            while temp.next is not None:
+                pre=temp
+                temp=temp.next
+            self.tail=pre 
+            self.tail.next=None
+            self.length-=1
+                
+                    
 my_linked_list=LinkedList(1)
 my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+
+
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length, '\n')
@@ -37,4 +59,6 @@ print('Linked List:')
 my_linked_list.print_list()
         
     
-        
+my_linked_list.pop()
+print("AFTER POPPING 2")
+my_linked_list.print_list()
